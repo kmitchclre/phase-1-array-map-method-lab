db963449bbbc2cdeb0fab92d75c6b04d5b06ed07
Kenny Clare <kmitchellclare@gmail.com>
2022-05-02 17:17:39 -0700

    commit tree 0a31f6f6a08ef76fc0d194273e0dec9529719986

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
+ 
 const titleCased = () => {
+  for(let i = 0; i < tutorials.length; i++) {
+    tutorials[0] = "What Does The This Keyword Mean?",
+    tutorials[1] = "What Is The Constructor OO Pattern?",
+    tutorials[2] = "Implementing Blockchain Web API",
+    tutorials[3] = "The Test Driven Development Workflow",
+    tutorials[4] = "What Is NaN And How Can We Check For It",
+    tutorials[5] = "What Is The Difference Between StopPropagation And PreventDefault?",
+    tutorials[6] = "Immutable State And Pure Functions"
+    tutorials[7] = "What Is The Difference Between == And ===?",
+    tutorials[8] = "What Is The Difference Between Event Capturing And Bubbling?",
+    tutorials[9] = "What Is JSONP?";
+  }
   return tutorials
 }
